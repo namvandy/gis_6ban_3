4d1155b564f5cd06a5c0ef8ad270b73304eb72d0
dhkang.bsn@gmail.com <85978564+dhkangBsnLikeLion@users.noreply.github.com>
2021-07-22 12:12:58 +0900

    2021-07-22 commit 6

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,9 +9,11 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
+has_ownership = [login_required, account_ownership_required]
 
 
 @login_required(login_url=reverse_lazy("accountapp:login"))
@@ -39,16 +41,15 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -57,8 +58,8 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     form_class = UserCreationForm
