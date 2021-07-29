542b97f918a943de333e3c8e1d19327d541b0a72
dhkang.bsn <dhkang.bsn@futureization.com>
2021-07-29 11:24:00 +0900

    2021-07-29 commit 14

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from profileapp.views import ProfileCreationView
+from profileapp.views import ProfileCreationView, ProfileUpdateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
     path('create/', ProfileCreationView.as_view(), name="create"),
-
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name="update"),
 ]
 
