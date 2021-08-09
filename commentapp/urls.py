cd5c58bf0ef6de87f580bd2b90b9c0a21fc9ce2c
dhkang.bsn <dhkang.bsn@gmail.com>
2021-08-09 11:42:57 +0900

    2021-08-09 commit 26

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from commentapp.views import CommentCreateView
+
 app_name = 'commentapp'
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-
+    path('create/', CommentCreateView.as_view(), name='create')
 ]
