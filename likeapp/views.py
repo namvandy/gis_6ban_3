188408bfefe1fb85fc1d4b1db27a2cf523693765
dhkang.bsn <dhkang.bsn@gmail.com>
2021-08-26 12:09:33 +0900

    2021-08-26 commit 52

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,38 +14,36 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    article.like += 1
+    article.save()
+
+    like_record = LikeRecord.objects.filter(user=user,
+                                            article=article)
+    if like_record.exists():
+        raise ValidationError('like already exists.')
+    else:
+        LikeRecord(user=user, article=article).save()
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
-    # RedirectView를 상속받았으므로, 어디로 갈지 지정해 주어야 한다.
-    # get_redirect_url로 지정해 줄 수 있다.
-
     def get(self, request, *args, **kwargs):
-        # 좋아요를 하기 위해서 해야 하는 작업
-        user = request.user # 유저를 받아 왔고 해당 유저로 로직을 처리하므로, login required는 필수이다.
-        article = Article.objects.get(pk=kwargs['article_pk']) # kwargs에 article_pk로 받아와야 한다. 즉, url로부터 받아와야 함.
+        user = request.user
+        article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record : list = LikeRecord.objects.filter(user=user, article=article)
-        if like_record.exists():
-            # 좋아요가 반영되지 않는 경우/ 좋아요 X -> error message 전달
-            # messages module
-            messages.add_message(request, messages.ERROR, '좋아요가 취소되었습니다.')
-            article.like -= 1
-            like_record.delete()
-            article.save()
-            ####
+        try:
+            db_transaction(user, article)
+            # 좋아요 반영 O
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
+        except ValidationError:
+            # 좋아요 반영 X
+            messages.add_message(request, messages.ERROR, '좋아요는 한번만 가능합니다.')
+            return HttpResponseRedirect(reverse('articleapp:detail',
+                                                kwargs={'pk': kwargs['article_pk']}))
 
-            return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-
-        article.like += 1
-        article.save() # 실제 db에 저장까지 해주어야 반영된다.
-
-        # 좋아요가 반영되는 경우 / 좋아요 O -> success message
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다.')
-        ####
-
-        return super().get(self, request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
-        return reverse('articleapp:detail', kwargs={'pk':kwargs['article_pk']})
+        return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
